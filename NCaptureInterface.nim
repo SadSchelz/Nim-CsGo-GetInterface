f7dd55f3967b532b9914a1bd6889dd2b14f0b991
Schelz <67759165+SadSchelz@users.noreply.github.com>
2022-10-19 21:34:52 +0300

    Add files via upload

---
+++ NCaptureInterface.nim
@@ -0,0 +1,28 @@
+{.passL: "-s -static-libgcc".}
+
+import winim
+
+type interface_fn* = proc (pName: cstring, pReturnCode: int): pointer {.cdecl.}   # change pointer with func type
+proc GetInterface*(dllName: cstring, interfaceName: cstring): auto {.cdecl.} =
+  let create_interface = cast[interface_fn](GetProcAddress(GetModuleHandleA(dllName), "CreateInterface"))
+  return create_interface(interfaceName, 0)
+
+proc mainThread(lpParameter: LPVOID) =
+  AllocConsole()
+  discard stdout.reopen("CONOUT$", fmWrite)
+
+  while GetAsyncKeyState(VK_END) == 0:
+    echo GetInterface("client.dll", "VClientEntityList003").repr        # return Interface pointer
+
+  stdout.close()
+  FreeConsole()
+  FreeLibraryAndExitThread(cast[HINSTANCE](lpParameter), 0)
+
+proc NimMain() {.cdecl, importc.}
+
+proc DllMain(hModule: HINSTANCE, reasonForCall: DWORD, lpReserved: LPVOID): WINBOOL {.exportc, dynlib, stdcall.} =
+  NimMain()
+  if reasonForCall == DLL_PROCESS_ATTACH:
+      DisableThreadLibraryCalls(hModule)
+      CloseHandle(CreateThread(cast[LPSECURITY_ATTRIBUTES](nil), 0.SIZE_T, cast[LPTHREAD_START_ROUTINE](mainThread), cast[LPVOID](hModule), 0.DWORD, cast[LPDWORD](nil)))
+  return TRUE
